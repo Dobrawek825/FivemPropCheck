dd80486f3ca0890d73bb77bddfd2c5703b538d06
Dobrawek825 <72352063+Dobrawek825@users.noreply.github.com>
2022-04-19 01:31:26 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'adamant'
+lua54 'yes'
+game 'gta5'
+
+client_script 'client.lua'
+server_script 'server.lua'
---
+++ server.lua
@@ -0,0 +1,28 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('propcheck')
+AddEventHandler('propcheck', function()
+    DropPlayer(source, 'Masz usunięte propy | Naprawiaj to szybko i wbijaj')
+end)
+
+RegisterNetEvent('Mlody.dobrawek:discord-Logs')
+AddEventHandler('Mlody.dobrawek:discord-Logs', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local nam = xPlayer.getName()
+    local dis = xPlayer.getIdentifier()
+    sendDiscord('**[Gracz]:** ' ..nam.. '\n**[Steam]:** ' ..dis..'')
+end)
+function sendDiscord(message)
+    local content = {
+        {
+            ["color"] = '16449791',
+            ["title"] = "Gracz chciał rozwalic wszystkich na bitkach bez propów ale mu nie wsyzło",
+            ["description"] = message,
+            ["footer"] = {
+                ["text"] = "Mlody.Dobrawek#2115 - Krzaki logs", 
+            },
+        }
+    }
+    PerformHttpRequest("WKLEJASZ SWÓJ WEBHOOK", function(err, text, headers) end, 'POST', json.encode({username = name, embeds = content}), { ['Content-Type'] = 'application/json' })
+end
